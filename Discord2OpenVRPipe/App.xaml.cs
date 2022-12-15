60500cf886e504363c03b3a5b953efa5d2e8c910
Jeppe Beier <jeppevinkel@gmail.com>
2022-12-15 17:03:42 +0100

    Blank slate for rewrite

--- Discord2OpenVRPipe/App.xaml.cs
+++ Discord2OpenVRPipe/App.xaml.cs
@@ -1,9 +1,17 @@
-﻿namespace Discord2OpenVRPipe
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Discord2OpenVRPipe
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App
+    public partial class App : Application
     {
     }
 }
